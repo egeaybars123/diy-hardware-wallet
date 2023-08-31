cd1c052d361bb231d5f9ceb0d6202575be344e06
Aegean <egeaybars1102@gmail.com>
2023-09-01 00:56:15 +0300

    modularize and improve logging

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,35 @@
 extern crate dotenv;
 
-use std::env;
 use dotenv::dotenv;
-use ethers::{utils, prelude::*};
+use std::env;
+use ethers::{prelude::*, utils};
+
+mod web3;
 
 //send to this address: 0xC57dA14667ECf7270348dcC7FB1E6D704e82D81e
+
+//hardware-wallet --to 0xC57dA14667ECf7270348dcC7FB1E6D704e82D81e --value 0.01 -> unsigned tx (rlp encoded) -> stm32 signing tx -> broadcast
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>>  {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
 
     let priv_key = env::var("PRIVATE_KEY").unwrap();
     let rpc_url = env::var("RPC_URL").unwrap();
 
-    //let signer = priv_key.parse::<LocalWallet>().unwrap();
-    //let address = signer.address();
-    //println!("{}", address);
-
     let provider = Provider::<Http>::try_from(rpc_url)?;
     let wallet = priv_key
-    .parse::<LocalWallet>()?
-    .with_chain_id(Chain::Sepolia);
+        .parse::<LocalWallet>()?
+        .with_chain_id(Chain::Sepolia);
 
     let client = SignerMiddleware::new(provider.clone(), wallet.clone());
 
     println!("{}", client.address());
 
-    let balance = provider.get_balance(client.address(), None).await?;
-    println!("{}", balance);
+    let balance = web3::get_balance(provider, client.address()).await?;
+    println!("Sepolia ETH Balance: {}", utils::format_units(balance, "ether")?);
 
-    let to_addr = "0xC57dA14667ECf7270348dcC7FB1E6D704e82D81e".parse::<Address>()?;
-    let tx = TransactionRequest::new()
-        .to(to_addr)
-        .value(U256::from(utils::parse_ether(0.01)?));
-    println!("{:?}", tx.rlp_unsigned());
+    let serialized_tx = web3::rlp_tx("0xC57dA14667ECf7270348dcC7FB1E6D704e82D81e", 0.01)?;
+    println!("{:?}", serialized_tx);
+
     Ok(())
 }
---
+++ src/web3.rs
@@ -0,0 +1,20 @@
+use ethers::{prelude::*, utils};
+
+//type Client = SignerMiddleware<Provider<Http>, Wallet<k256::ecdsa::SigningKey>>;
+
+pub async fn get_balance(
+    provider: Provider<Http>,
+    to_addr: Address,
+) -> Result<U256, Box<dyn std::error::Error>> {
+    let balance = provider.get_balance(to_addr, None).await?;
+    Ok(balance)
+}
+
+pub fn rlp_tx(to_addr: &str, value: f64) -> Result<Bytes, Box<dyn std::error::Error>> {
+    let to_addr = to_addr.parse::<Address>()?;
+    let tx = TransactionRequest::new()
+        .to(to_addr)
+        .value(U256::from(utils::parse_ether(value)?));
+
+    Ok(tx.rlp_unsigned())
+}
