401b68b905552a2a7249bc9a0a900ae8ed4152ca
Aegean <egeaybars1102@gmail.com>
2023-09-03 11:17:01 +0300

    prepare tx

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ extern crate dotenv;
 
 use dotenv::dotenv;
 use std::env;
-use ethers::{prelude::*, utils};
+use ethers::{prelude::*, utils, providers::Middleware};
 
 mod web3;
 
@@ -21,15 +21,34 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .parse::<LocalWallet>()?
         .with_chain_id(Chain::Sepolia);
 
+    
+    let nonce = provider.get_transaction_count(wallet.address(), None).await?;
+    println!("Nonce: {}", nonce);
+
     let client = SignerMiddleware::new(provider.clone(), wallet.clone());
 
     println!("{}", client.address());
 
-    let balance = web3::get_balance(provider, client.address()).await?;
+    let balance = web3::get_balance(&provider, client.address()).await?;
     println!("Sepolia ETH Balance: {}", utils::format_units(balance, "ether")?);
 
-    let serialized_tx = web3::rlp_tx("0xC57dA14667ECf7270348dcC7FB1E6D704e82D81e", 0.01)?;
-    println!("{:?}", serialized_tx);
+    //let serialized_tx = web3::rlp_tx("0xC57dA14667ECf7270348dcC7FB1E6D704e82D81e", 0.01)?;
+    //println!("{:?}", serialized_tx);
+    //wallet.sign_transaction_sync(tx)
+
+    //add typed transaction and tx.sig_hash() which will send the hash to the stm32 for signing.
+    //prepare tx.
+
+    let price_gas = provider.get_gas_price().await?;
+    
+    let tx = TransactionRequest::new()
+    .nonce(nonce)
+    .to("0xC57dA14667ECf7270348dcC7FB1E6D704e82D81e".parse::<Address>()?)
+    .value(U256::from(utils::parse_ether(0.001)?))
+    .gas_price(price_gas)
+    .gas(21000)
+    .chain_id(Chain::Sepolia);
+
 
     Ok(())
 }
--- src/web3.rs
+++ src/web3.rs
@@ -3,7 +3,7 @@ use ethers::{prelude::*, utils};
 //type Client = SignerMiddleware<Provider<Http>, Wallet<k256::ecdsa::SigningKey>>;
 
 pub async fn get_balance(
-    provider: Provider<Http>,
+    provider: &Provider<Http>,
     to_addr: Address,
 ) -> Result<U256, Box<dyn std::error::Error>> {
     let balance = provider.get_balance(to_addr, None).await?;
